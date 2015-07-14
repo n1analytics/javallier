61a0cf4da02603fa6a6e4590bfa3d9d585163d17
whenecka <wilko.henecka@nicta.com.au>
2015-07-14 14:19:33 +1000

    improved encryption function

--- src/main/java/com/n1analytics/paillier/PaillierContext.java
+++ src/main/java/com/n1analytics/paillier/PaillierContext.java
@@ -355,12 +355,12 @@ public final class PaillierContext {
   }
 
   public EncryptedNumber encrypt(EncodedNumber encoded) {
-    // TODO optimise
+
     checkSameContext(encoded);
-    final BigInteger generator = publicKey.getGenerator();
+    final BigInteger modulus = publicKey.getModulus();
     final BigInteger modulusSquared = publicKey.getModulusSquared();
     final BigInteger value = encoded.getValue();
-    final BigInteger result = generator.modPow(value, modulusSquared);
+    final BigInteger result = modulus.multiply(value).add(BigInteger.ONE).mod(modulusSquared);
     return new EncryptedNumber(this, result, encoded.getExponent());
   }
 
