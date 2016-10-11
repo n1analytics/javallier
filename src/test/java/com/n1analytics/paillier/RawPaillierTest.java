e88918da36b6460707ad6e85ef647785d973365c
Wilko <wilko.henecka@data61.csiro.au>
2016-10-11 19:31:00 +1100

    randomised tests

--- src/test/java/com/n1analytics/paillier/RawPaillierTest.java
+++ src/test/java/com/n1analytics/paillier/RawPaillierTest.java
@@ -3,6 +3,8 @@ package com.n1analytics.paillier;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.n1analytics.paillier.util.BigIntegerUtil;
+
 import java.math.BigInteger;
 
 import static org.junit.Assert.assertEquals;
@@ -12,56 +14,56 @@ public class RawPaillierTest {
 
   static PaillierPrivateKey privateKey;
   static PaillierPublicKey publicKey;
+  static final int maxIterations = TestConfiguration.MAX_ITERATIONS;
   
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    privateKey = PaillierPrivateKey.create(2048);
+    privateKey = PaillierPrivateKey.create(TestConfiguration.DEFAULT_KEY_SIZE);
     publicKey = privateKey.getPublicKey();
   }
   
   @Test
   public void testEncryptionDecryption(){
-    BigInteger plaintext = new BigInteger("42");
-    BigInteger ciphertext = publicKey.raw_encrypt(plaintext);
-    assertNotEquals(plaintext, ciphertext);
-    assertEquals(plaintext, privateKey.raw_decrypt(ciphertext));
+    for (int i = 0; i < maxIterations; i++) {
+      BigInteger plaintext = BigIntegerUtil.randomPositiveNumber(publicKey.getModulus());
+      BigInteger ciphertext = publicKey.raw_encrypt(plaintext);
+      assertNotEquals(plaintext, ciphertext);
+      assertEquals(plaintext, privateKey.raw_decrypt(ciphertext));
+    }
   }
   
   @Test
   public void testAdd(){
-    BigInteger a = new BigInteger("123");
-    BigInteger b = new BigInteger("7654");
-    BigInteger ciphertext = publicKey.raw_add(publicKey.raw_encrypt(a), publicKey.raw_encrypt(b));
-    assertEquals(privateKey.raw_decrypt(ciphertext), a.add(b));
-    //test overflow
-    a = publicKey.modulus;
-    b = BigInteger.ONE;
-    ciphertext = publicKey.raw_add(publicKey.raw_encrypt(a), publicKey.raw_encrypt(b));
-    assertEquals(privateKey.raw_decrypt(ciphertext), BigInteger.ONE);
+    for (int i = 0; i < maxIterations; i++) {
+      BigInteger a = BigIntegerUtil.randomPositiveNumber(publicKey.getModulus());
+      BigInteger b = BigIntegerUtil.randomPositiveNumber(publicKey.getModulus());
+      BigInteger ciphertext = publicKey.raw_add(publicKey.raw_encrypt(a), publicKey.raw_encrypt(b));
+      assertEquals(privateKey.raw_decrypt(ciphertext), a.add(b).mod(publicKey.getModulus()));
+    }
   }
   
   @Test
   public void testMul(){
-    BigInteger a = new BigInteger("95831");
-    BigInteger k = BigInteger.ONE;
-    BigInteger ciphertext = publicKey.raw_multiply(publicKey.raw_encrypt(a), k);
-    assertEquals(privateKey.raw_decrypt(ciphertext), a);
-    k = new BigInteger("842");
-    ciphertext = publicKey.raw_multiply(publicKey.raw_encrypt(a), k);
-    assertEquals(privateKey.raw_decrypt(ciphertext), a.multiply(k));
-    a = publicKey.modulus.subtract(BigInteger.ONE);
-    k = new BigInteger("42");
-    ciphertext = publicKey.raw_multiply(publicKey.raw_encrypt(a), k);
-    assertEquals(privateKey.raw_decrypt(ciphertext), a.multiply(k).mod(publicKey.modulus));
+    for (int i = 0; i < maxIterations; i++) {
+      BigInteger a = BigIntegerUtil.randomPositiveNumber(publicKey.getModulus());
+      BigInteger k = BigInteger.ONE;
+      BigInteger ciphertext = publicKey.raw_multiply(publicKey.raw_encrypt(a), k);
+      assertEquals(privateKey.raw_decrypt(ciphertext), a);
+      k = BigIntegerUtil.randomPositiveNumber(BigInteger.valueOf(1000));
+      ciphertext = publicKey.raw_multiply(publicKey.raw_encrypt(a), k);
+      assertEquals(privateKey.raw_decrypt(ciphertext), a.multiply(k).mod(publicKey.getModulus()));     
+    }
   }
   
   @Test
   public void testObfuscate(){
-    BigInteger a = new BigInteger("123456789");
-    BigInteger ciphertext = publicKey.raw_encrypt(a);
-    BigInteger obuscatedCiphertext = publicKey.raw_obfuscate(ciphertext);
-    assertNotEquals(ciphertext, obuscatedCiphertext);
-    assertEquals(privateKey.raw_decrypt(obuscatedCiphertext), a);
+    for (int i= 0; i < maxIterations; i++) {
+      BigInteger a = BigIntegerUtil.randomPositiveNumber(publicKey.getModulus());
+      BigInteger ciphertext = publicKey.raw_encrypt(a);
+      BigInteger obuscatedCiphertext = publicKey.raw_obfuscate(ciphertext);
+      assertNotEquals(ciphertext, obuscatedCiphertext);
+      assertEquals(privateKey.raw_decrypt(obuscatedCiphertext), a);
+    }
   }
   
 }
